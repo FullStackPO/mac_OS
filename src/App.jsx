fc7e7e541d314d15d48e273d8f41ff4f3ff4b047
Harsh Arya <harsharya8946@gmail.com>
2026-04-28 22:27:27 +0530

    mac app interface

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import './app.scss'
 import Dock from './components/Dock'
 import Navbar from './components/Navbar'
+import MacWindow from './windows/MacWindow'
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <MacWindow>
+      </MacWindow>
       <Dock />
     </main>
   )
---
+++ src/windows/MacWindow.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Rnd } from 'react-rnd'
+import '../window.scss'
+
+const MacWindow = ({children}) => {
+  return (
+   <Rnd>
+    <div className="window">
+        <div className="nav">
+        <div className="dots">
+        <div className="dot red"></div>
+        <div className="dot yellow"></div>
+        <div className="dot green"></div>
+        </div>
+        <div className="owner"><p>Harsh~Arya</p></div>
+        </div>
+        <div className="content">
+            {children}
+        </div>
+    </div>
+   </Rnd>
+  )
+}
+
+export default MacWindow
