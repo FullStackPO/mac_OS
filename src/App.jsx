c4150e80eb82c17f739bf26232b7c76909ae290b
Harsh Arya <harsharya8946@gmail.com>
2026-04-30 23:50:51 +0530

    notes section & scrollbar fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './app.scss'
 import Dock from './components/Dock'
 import Navbar from './components/Navbar'
 import Github from './windows/Github'
+import Notes from './windows/Notes'
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Github />
+      <Notes />
       <Dock />
     </main>
   )
